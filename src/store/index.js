1b3dd1045b4c616054c45ca6f725ee6ebc198a94
Peter Cervienka <peter.cervienka@itera.no>
2018-04-23 17:58:45 +0200

    feat: using rematch to init redux stores

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { init } from "@rematch/core";
 
 import { reducers } from '../reducers';
 
-const reducer = combineReducers({
-  ...reducers,
+export const store = init({
+  redux: {
+    reducers: reducers,
+    middlewares: [thunk]
+  },
 });
-
-const finalCreateStore = compose(
-  applyMiddleware(thunk)
-)(createStore);
-
-export const store = finalCreateStore(reducer);
 
